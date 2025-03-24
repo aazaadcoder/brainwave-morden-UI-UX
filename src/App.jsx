cc6b7cd248b9ab984af60fcdf678494331e6901b
aazaadcoder <128458378+aazaadcoder@users.noreply.github.com>
2025-03-24 21:15:54 +0530

    add the bento box section after the services section

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,16 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
+import Services from "./components/Services";
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
-        <Hero/>
-        <Benefits/>
-        <Collaboration/>
+        <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
       </div>
       <ButtonGradient />
     </>
--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
-const Heading = ({ className, title }) => {
-  return (<div className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20`}>{title && <h2 className="h2">{title}</h2>}</div>);
+const Heading = ({ className, title, text }) => {
+  return (<div className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20`}>{title && <h2 className="h2">{title}</h2>}
+  {text && <p className="body-2 m-4 text-n-4">{text}</p>}
+  </div>);
 };
 
 export default Heading;
