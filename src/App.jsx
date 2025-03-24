fc9aa65c766dc3c3f80a94f0c8b6ef47f1e16e45
aazaadcoder <128458378+aazaadcoder@users.noreply.github.com>
2025-03-24 23:09:50 +0530

    add the footer and the roadmap section , but the roadmap section has issues and is not complete

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
 import Services from "./components/Services";
 import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
+import Footer from "./components/Footer";
 const App = () => {
   return (
     <>
@@ -17,6 +19,8 @@ const App = () => {
         <Collaboration />
         <Services />
         <Pricing/>
+        <Roadmap/>
+        <Footer/>
       </div>
       <ButtonGradient />
     </>
